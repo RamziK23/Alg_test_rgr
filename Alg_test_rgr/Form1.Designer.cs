d57f1bfbb3cd0579786f61b79726cad8ac4e47e3
RamziK23 <ramzesdavletbaev23@gmail.com>
2020-12-19 16:11:04 +0500

    Initial commit

---
+++ Alg_test_rgr/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Alg_test_rgr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel = new System.Windows.Forms.Panel();
+            this.btn_prim = new System.Windows.Forms.Button();
+            this.btn_clean = new System.Windows.Forms.Button();
+            this.dgv_matrix = new System.Windows.Forms.DataGridView();
+            this.textbox_result = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_matrix)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Location = new System.Drawing.Point(12, 112);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(497, 426);
+            this.panel.TabIndex = 0;
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_MouseDown);
+            // 
+            // btn_prim
+            // 
+            this.btn_prim.Location = new System.Drawing.Point(12, 12);
+            this.btn_prim.Name = "btn_prim";
+            this.btn_prim.Size = new System.Drawing.Size(75, 23);
+            this.btn_prim.TabIndex = 2;
+            this.btn_prim.Text = "Поиск";
+            this.btn_prim.UseVisualStyleBackColor = true;
+            this.btn_prim.Click += new System.EventHandler(this.btn_prim_Click);
+            // 
+            // btn_clean
+            // 
+            this.btn_clean.Location = new System.Drawing.Point(93, 12);
+            this.btn_clean.Name = "btn_clean";
+            this.btn_clean.Size = new System.Drawing.Size(75, 23);
+            this.btn_clean.TabIndex = 3;
+            this.btn_clean.Text = "Очистить";
+            this.btn_clean.UseVisualStyleBackColor = true;
+            this.btn_clean.Click += new System.EventHandler(this.btn_clean_Click);
+            // 
+            // dgv_matrix
+            // 
+            this.dgv_matrix.AllowUserToAddRows = false;
+            this.dgv_matrix.AllowUserToDeleteRows = false;
+            this.dgv_matrix.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgv_matrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_matrix.Location = new System.Drawing.Point(515, 112);
+            this.dgv_matrix.Name = "dgv_matrix";
+            this.dgv_matrix.ReadOnly = true;
+            this.dgv_matrix.Size = new System.Drawing.Size(521, 346);
+            this.dgv_matrix.TabIndex = 4;
+            // 
+            // textbox_result
+            // 
+            this.textbox_result.Location = new System.Drawing.Point(12, 41);
+            this.textbox_result.Name = "textbox_result";
+            this.textbox_result.Size = new System.Drawing.Size(156, 20);
+            this.textbox_result.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btn_prim);
+            this.panel1.Controls.Add(this.textbox_result);
+            this.panel1.Controls.Add(this.btn_clean);
+            this.panel1.Location = new System.Drawing.Point(515, 464);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 74);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Об авторе: Давлетбаев Рамзес Рустемович, группа ПРО-222";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1027, 32);
+            this.label2.TabIndex = 15;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 550);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgv_matrix);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_matrix)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btn_prim;
+        private System.Windows.Forms.Button btn_clean;
+        private System.Windows.Forms.DataGridView dgv_matrix;
+        private System.Windows.Forms.TextBox textbox_result;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Alg_test_rgr/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Microsoft.VisualBasic;
+
+namespace Alg_test_rgr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        LinkedList<Circle> circles = new LinkedList<Circle>();
+        int vertex = -1;
+        bool check = false;
+        int ves; // вес ребра
+        int n = 0;//vertex count
+
+        class Circle
+        {
+            private int x, y;
+            private int rad = 15;
+            public int X { get { return x; } set { x = value; } }
+            public int Y { get { return y; } set { y = value; } }
+            public int Rad { get { return rad; } }
+        }
+
+        Point[] koortoch = new Point[50]; //массив точек
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            Circle circle = new Circle();
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    Pen pen_circle = new Pen(Color.Black, 3);
+                    circle.X = e.X - circle.Rad;
+                    circle.Y = e.Y - circle.Rad;
+                    panel.CreateGraphics().DrawEllipse(pen_circle, circle.X, circle.Y,
+                        circle.Rad * 2, circle.Rad * 2);
+
+                    String drawString = (circles.Count + 1).ToString();
+                    int fontSize = 14;
+                    if (circles.Count + 1 >= 10)
+                        fontSize = 11;
+
+                    Font font = new Font("Arial", fontSize);
+                    SolidBrush brush = new SolidBrush(Color.Black);
+                    PointF point = new PointF(circle.X + circle.Rad / 2, (circle.Y + circle.Rad / 2) - 2);
+
+                    panel.CreateGraphics().DrawString(drawString, font, brush, point);
+                    circles.AddLast(circle);
+                    n++;
+                    koortoch[n] = e.Location;
+                    if (check == false)
+                    {
+                        var column1 = new DataGridViewColumn();
+                        column1.HeaderText = "0";
+                        column1.ReadOnly = true;
+                        column1.Name = "0";
+                        column1.CellTemplate = new DataGridViewTextBoxCell();
+                        column1.DefaultCellStyle.BackColor = Color.White;
+                        column1.DefaultCellStyle.ForeColor = Color.Black;
+                        dgv_matrix.Columns.Add(column1);
+                        dgv_matrix.Rows.Add();
+                        dgv_matrix.Columns.Add(1.ToString(), 1.ToString());
+                        dgv_matrix[0, 0].Value = 1;
+                        check = true;
+                    }
+                    else
+                    {
+                        for (int i = circles.Count; i < circles.Count + 1; ++i)
+                        {
+                            dgv_matrix.Columns.Add(i.ToString(), i.ToString());
+                            dgv_matrix.Rows.Add();
+                        }
+                        for (int i = 0; i < circles.Count; ++i)
+                            dgv_matrix[0, i].Value = i + 1;
+                    }
+                    break;
+
+                case MouseButtons.Right:
+                    Pen pen_selected = new Pen(Color.Red, 2);
+                    if (vertex == -1)
+                    {
+                        int count = 0;
+                        foreach (Circle c in circles)
+                        {
+                            if (e.X - c.Rad <= c.X + c.Rad &&
+                                      e.X - c.Rad >= c.X - c.Rad &&
+                                   e.Y - c.Rad <= c.Y + c.Rad &&
+                                   e.Y - c.Rad >= c.Y - c.Rad)
+                            {
+                                vertex = count;
+                                panel.CreateGraphics().DrawEllipse(pen_selected, c.X, c.Y, c.Rad * 2, c.Rad * 2);
+                                break;
+                            }
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        int tovertex = -1;
+                        int count = 0;
+                        foreach (Circle c in circles)
+                        {
+                            if (e.X - c.Rad <= c.X + c.Rad &&
+                                        e.X - c.Rad >= c.X - c.Rad &&
+                                        e.Y - c.Rad <= c.Y + c.Rad &&
+                                        e.Y - c.Rad >= c.Y - c.Rad)
+                            {
+                                tovertex = count;
+                                panel.CreateGraphics().DrawEllipse(pen_selected, c.X, c.Y, c.Rad * 2, c.Rad * 2);
+                                break;
+                            }
+                            count++;
+                        }
+
+                        if ((tovertex != -1) && (vertex != tovertex))
+                        {
+                            int it = 0;
+                            Point p1 = new Point(0, 0);
+                            Point p2 = new Point(0, 0);
+                            foreach (Circle c in circles)
+                            {
+                                if (it == vertex)
+                                    p1 = new Point(c.X + c.Rad, c.Y + c.Rad);
+                                if (it == tovertex)
+                                    p2 = new Point(c.X + c.Rad, c.Y + c.Rad);
+                                it++;
+                            }
+                            panel.CreateGraphics().DrawLine(pen_selected, p1, p2);
+                            try
+                            {
+                                ves = Convert.ToInt32(Interaction.InputBox("Введите вес", "Вес", "", 100, 100));
+                            }
+                            catch (Exception) // если возникла ошибка
+                            {
+                                ves = 1;
+                            }
+
+                            for (int i = 0; i < n; i++)
+                            {
+                                for (int j = 0; j < n; j++)
+                                {
+                                    Pen pen_edge = new Pen(Color.Black, 3);
+                                    var label_edge = new Label //определение Label для визуализации расстояния между городами
+                                    {
+                                        Text = ves.ToString(), //расстояние
+                                        Location = new Point((p1.X + p2.X) / 2, ((p1.Y + p2.Y) / 2) - 20), //координаты размещения Label
+                                        AutoSize = true,
+                                        Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))),
+                                        ForeColor = Color.Black,
+                                    };
+                                    panel.Controls.Add(label_edge); //добавление Label на полотно
+                                    label_edge.BringToFront(); //перенос Label поверх остальных элементов на полотне
+                                }
+                            }
+
+                            for (int i = circles.Count; i < circles.Count + 1; ++i)
+                            {
+                                dgv_matrix[vertex + 1, tovertex].Value = ves;
+                                dgv_matrix[tovertex + 1, vertex].Value = ves;
+                            }
+                            vertex = -1;
+                        }
+                    }
+                    break;
+            }
+        }
+        private void btn_prim_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n + 1; i++)
+                for (int j = 0; j < n; j++)
+                    if (dgv_matrix[i, j].Value == null)
+                        dgv_matrix[i, j].Value = 0;
+            int[,] copymatrix = new int[n + 1, n + 1]; //двумерный массив, копирующий dataGridView
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    copymatrix[i, j] = Convert.ToInt32(dgv_matrix[i + 1, j].Value);
+                }
+            }
+            int no_edge;
+            int result = 0;
+            int[] selected = new int[n + 1];
+            List<int> list = new List<int>();
+
+            no_edge = 0;
+            int x, y;
+            selected[0] = 1;
+            while (no_edge <= n)
+            {
+                int min = Int32.MaxValue;                //int min = 10000000;
+                x = 0;
+                y = 0;
+                for (int i = 0; i < n + 1; i++)
+                {
+                    if (selected[i] == 1)
+                    {
+                        for (int j = 0; j < n + 1; j++)
+                        {
+                            if ((selected[j] != 1) && (copymatrix[i, j] != 0))
+                            {
+                                if (min > copymatrix[i, j])
+                                {
+                                    min = copymatrix[i, j];
+                                    x = i;
+                                    y = j;
+
+                                }
+                            }
+                        }
+                    }
+                }
+                if (min == Int32.MaxValue)
+                    min = 0;
+                if (min != 0)
+                    list.Add(min);
+                result += min;
+                selected[y] = 1;
+                no_edge++;
+                textbox_result.Text = "Вес остова =" + result + " (Ребра ";
+                foreach (int i in list)
+                    textbox_result.Text += i.ToString() + "  ";
+                textbox_result.Text += ")";
+            }
+        }
+
+        private void btn_clean_Click(object sender, EventArgs e)
+        {
+            textbox_result.Text = "";
+            dgv_matrix.Columns.Clear();
+            dgv_matrix.Rows.Clear();
+            panel.Refresh();
+            circles.Clear();
+            dgv_matrix.Columns.Add(0.ToString(), 0.ToString());
+            n = 0;
+        }
+
+    }
+}
